4fa108324ae3caecd7af315be1764373f0fd6fe7
TanaySrivas <tanaysriofficial@gmail.com>
2023-11-21 09:07:54 +0530

    Armchairs code fixed

--- Armchairs.java
+++ Armchairs.java
@@ -21,20 +21,18 @@ public class Armchairs {
         }
         int n1 = seated.size() - 1;
         int n2 = emptySeats.size() - 1;
-        int dp[][] = new int[n1+1][n2+1];
-        for(int[] row: dp)
-            Arrays.fill(row, Integer.MAX_VALUE);
-        dp[0][0] = 0;
-        for(int i=0;i<=n1;i++){
+        long dp[][] = new long[n1+1][n2+1];
+        for(int i=1;i<=n1;i++){
+            for(int j=0;j<=n2;j++){
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+        for(int i=1;i<=n1;i++){
             for(int j=1;j<=n2;j++){
-                if(i > 0){
-                    dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1] + Math.abs(seated.get(i) - emptySeats.get(j)));
-                }
+                dp[i][j] = Math.min(dp[i][j], dp[i-1][j-1] + Math.abs(seated.get(i) - emptySeats.get(j)));
                 dp[i][j] = Math.min(dp[i][j], dp[i][j-1]);
             }
         }
         System.out.println(dp[n1][n2]);
-        for(int[] row: dp)
-            System.out.println(Arrays.toString(row));
     }
 }
