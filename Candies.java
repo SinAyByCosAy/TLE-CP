9788779437d3e63e69c5702655b5f1da7bf78618
TanaySrivas <tanaysriofficial@gmail.com>
2023-11-29 13:18:29 +0530

    Candies code with Prefix Sum

--- Candies.java
+++ Candies.java
@@ -15,20 +15,29 @@ public class Candies {
             a[i] = sc.nextInt();
         }
         int dp[][] = new int[n+1][k+1];
+        long PSum[][] = new long[n+1][k+1];
         for(int j=0;j<=a[1];j++)
             dp[1][j] = 1;
+        PSum[1][0] = dp[1][0];
+        for(int j=1;j<=k;j++)
+            PSum[1][j] = PSum[1][j-1] + dp[1][j];
         for(int i=2;i<=n;i++){
             for(int j=0;j<=k;j++){
                 long sum = 0;
-                for(int pick=0;pick<=a[i];pick++){
-                    if(j-pick < 0) break;
-                    sum = (sum + dp[i-1][j-pick]) % m;
-                }
+                int idx = j - a[i];
+                if(idx <= 0)
+                    sum = PSum[i-1][j];
+                else
+                    sum = PSum[i-1][j] - PSum[i-1][idx-1];
                 dp[i][j] = (int)sum;
+                PSum[i][j] = (j == 0) ? dp[i][j] : (PSum[i][j-1] + dp[i][j])%m;
             }
         }
         System.out.println(dp[n][k]);
-        for(int[] row: dp)
-            System.out.println(Arrays.toString(row));
+//        for(int[] row: dp)
+//            System.out.println(Arrays.toString(row));
+//        System.out.println();
+//        for(int[] row: PSum)
+//            System.out.println(Arrays.toString(row));
     }
 }
