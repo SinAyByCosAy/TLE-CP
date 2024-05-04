42bf462c6d4683c6b2a5027c2345ab3b87c89b0c
TanaySrivas <tanaysriofficial@gmail.com>
2024-05-04 16:31:14 +0530

    sqrt bs

---
+++ BinarySearch/sqrt.java
@@ -0,0 +1,22 @@
+package DPBootcamp.BinarySearch;
+
+import java.util.Scanner;
+
+public class sqrt {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int s = 1, e = x, m;
+        int ans = 1;
+        while(s <= e){
+            m = (s + e)/2;
+            if(m * m <= x){
+                ans = m;
+                s = m + 1;
+            }else{
+                e = m - 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}
