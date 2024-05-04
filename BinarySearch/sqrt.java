91f7748fc1af1f3120a1e25c37bb2d5bae726fe0
TanaySrivas <tanaysriofficial@gmail.com>
2024-05-04 16:38:31 +0530

    sqrt overflow handled

--- BinarySearch/sqrt.java
+++ BinarySearch/sqrt.java
@@ -10,7 +10,7 @@ public class sqrt {
         int ans = 1;
         while(s <= e){
             m = (s + e)/2;
-            if(m * m <= x){
+            if(m <= (x/m)){
                 ans = m;
                 s = m + 1;
             }else{
