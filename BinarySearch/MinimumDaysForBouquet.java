041647e6690fac953f664b121662edf6a3b749bb
TanaySrivas <tanaysriofficial@gmail.com>
2024-05-14 14:06:06 +0530

    Min days for a bouquet done

---
+++ BinarySearch/MinimumDaysForBouquet.java
@@ -0,0 +1,61 @@
+package DPBootcamp.BinarySearch;
+
+import java.awt.event.MouseAdapter;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Scanner;
+
+public class MinimumDaysForBouquet {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+            max = Math.max(max, arr[i]);
+        }
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(minDays(arr, m, k, max));
+    }
+    public static int minDays(int[] bloomDay, int m, int k, int max){
+        if(1l*m*k > bloomDay.length) return -1;
+        int start = 1, end = max;
+        int ans = end;
+        while(start <= end){
+            int mid = (start + end) / 2;
+            if(check(bloomDay, m, k, mid)){
+                ans = mid;
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static boolean check(int[] bloomDay, int m, int k, int target){
+        int n = bloomDay.length;
+        int eleCount = 0;//elements in current window
+        int bouquetCount = 0;//buquets made
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i=0;i<n;i++){
+            while(!dq.isEmpty() && dq.peek() <= i-k) dq.poll();
+            while(!dq.isEmpty() && bloomDay[dq.peekLast()] <= bloomDay[i]) dq.pollLast();
+            dq.offer(i);
+            eleCount++;
+            if(eleCount == k){
+                if(bloomDay[dq.peek()] <= target){
+                    bouquetCount++;
+                    dq.clear();
+                    eleCount = 0;
+                }else{
+                    eleCount--;
+                }
+            }
+        }
+        if(bouquetCount >= m)
+            return true;
+        return false;
+    }
+}
