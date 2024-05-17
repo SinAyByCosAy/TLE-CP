977589d33bea21d475ddd69d70a5914b64589dfe
TanaySrivas <tanaysriofficial@gmail.com>
2024-05-18 03:02:08 +0530

    Binary Search based on count

--- BinarySearch/FindDuplicateNumber.java
+++ BinarySearch/FindDuplicateNumber.java
@@ -13,6 +13,7 @@ public class FindDuplicateNumber {
         }
 //        System.out.println(findDuplicateArrayModify(arr));
         System.out.println(findDuplicateCycleNode(arr));
+        System.out.println(findDuplicateBS(arr));
     }
 
     //Approach: Consider elements as addresses like in LL. Duplicate element will create a cycle. Find loop node, Floyd's cycle detection algo.
@@ -53,5 +54,29 @@ public class FindDuplicateNumber {
             }
         }
         return -1;
+    }
+
+    //Approach: Binary Search based on count
+    // Count of elements <= X should be <= X
+    // [1,2,3,4,4,5] elements <= 1 are 1, elements <= 2 are 2, elements <= 3 are 3
+    // but elements <= 4 are 5 as it has duplicate, elements <= 5 are 6, elements <=6 are 7
+    // therefore, if we have an element X where count > X, then there is a duplicate element in its range
+    public static int findDuplicateBS(int[] nums){
+        int start = 1, end = nums.length - 1;
+        int ans = -1;
+        while (start <= end){
+            int mid = (start + end) / 2;
+            int count = 0;
+            for (int num : nums) {
+                if (num <= mid) count++;
+            }
+            if(count <= mid){
+                start = mid + 1;
+            }else{
+                ans = mid;
+                end = mid - 1;
+            }
+        }
+        return ans;
     }
 }
