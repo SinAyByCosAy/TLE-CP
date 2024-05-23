38fd286f341c0a57fb8a1edae1d4ddb654c064bb
TanaySrivas <tanaysriofficial@gmail.com>
2024-05-23 16:38:59 +0530

    BS on distance

--- BinarySearch/CellularNetwork.java
+++ BinarySearch/CellularNetwork.java
@@ -21,7 +21,10 @@ public class CellularNetwork {
             minPossibleDistance = Math.max(minPossibleDistance, getDistance(towers, cities[i]));
         }
         System.out.println(minPossibleDistance);
+        System.out.println(binarySearchAnswer(cities, towers));
     }
+
+    //Approach 1: For each city, find it's position among towers using Binary Search
     public static int getDistance(int[] towers, int city){ //find location of city in towers list
         int start = 0, end = towers.length - 1;
         int pos = towers.length;
@@ -40,5 +43,30 @@ public class CellularNetwork {
         if(pos != towers.length)
             distance = Math.min(distance, towers[pos] - city);
         return distance;
-    }
+    }//TC:O(N * log M)
+
+    //Approach 2: Binary Search on answer that is possible distance
+    public static long binarySearchAnswer(int[] cities, int[] towers){
+        int n = cities.length, m = towers.length;
+        long start = 0, end = (long)2e9;
+        long ans = end;
+        while(start <= end){
+            long mid = (start + end) / 2; //test distance
+            int i=0, j=0;
+            while(i<n && j<m){
+                if(Math.abs(cities[i] - towers[j]) <= mid){//city can get connected to current tower with the specified distance
+                    i++;
+                }else{//trying the next tower
+                    j++;
+                }
+            }
+            if(i == n){//all cities got connection
+                ans = mid;
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }//TC:O(N * log 2e9) or //M = max(tower pos)-min(city pos) or max(city pos)-min(tower pos) TC:O(N * log M)
 }
