5bcf972cbc587c9c2e98628656dace934246a339
TanaySrivas <tanaysriofficial@gmail.com>
2024-06-21 14:05:19 +0530

    longest substring without repeating chars

--- SlidingWindow/LongestSubstringNoRepeatingChars.java
+++ SlidingWindow/LongestSubstringNoRepeatingChars.java
@@ -1,5 +1,7 @@
+//https://leetcode.com/problems/longest-substring-without-repeating-characters/
 package DPBootcamp.SlidingWindow;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
@@ -8,24 +10,19 @@ public class LongestSubstringNoRepeatingChars {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         int n = s.length();
-        HashMap<Character, Integer> hm = new HashMap<>();
-        int p1 = 0;
+        int l = 0, r = 0;
         int maxLen = 0;
-        for(int p2 = 0; p2 <= n; p2++){
-            if(p2 == n){
-                maxLen = Math.max(maxLen, p2 - p1);
-
-            }else if(hm.containsKey(s.charAt(p2))){
-                if(hm.get(s.charAt(p2)) < p1)
-                    hm.put(s.charAt(p2), p2);
-                else {
-                    maxLen = Math.max(maxLen, p2 - p1);
-                    p1 = hm.get(s.charAt(p2)) + 1;
-                    hm.put(s.charAt(p2), p2);
-                }
-            }else{
-                hm.put(s.charAt(p2), p2);
+        int[] map = new int[256]; //map for 256 ASCII chars
+        Arrays.fill(map, -1);
+        while(r < n){
+            char curr = s.charAt(r);
+            if(map[curr] != -1 && map[curr] >= l){ //if we have seen the char before and if it's present in the current range
+                l = map[curr] + 1; //we shift our left end forward to remove duplication and look for a better answer
             }
+            int len = r - l + 1;
+            maxLen = Math.max(maxLen, len);
+            map[curr] = r; //update current position
+            r++;
         }
         System.out.println(maxLen);
     }
