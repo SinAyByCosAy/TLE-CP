fa81f13f8db80c1e0b1ba3b70f0646cbf0b0caa2
TanaySrivas <tanaysriofficial@gmail.com>
2024-08-11 02:31:17 +0530

    Updated comparators

--- Templates/ImpFunctions.java
+++ Templates/ImpFunctions.java
@@ -1,5 +1,9 @@
 package DPBootcamp.Templates;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class ImpFunctions {
 
     //Random value generator from [a, b]
@@ -9,5 +13,13 @@ public class ImpFunctions {
 
     public static int gcd(int a, int b){
         return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public static void sortList(List<Integer> arr){
+        Collections.sort(arr, (a, b) -> Integer.compare(a, b));
+    }
+
+    public static void sortArray(Integer[] arr){
+        Arrays.sort(arr, (a, b) -> Integer.compare(a, b)); //requires an object type
     }
 }
