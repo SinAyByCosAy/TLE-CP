36dee9897b579844540ead9a7847c92c279faa50
TanaySrivas <tanaysriofficial@gmail.com>
2024-08-12 09:53:17 +0530

    Modulo division template

--- Templates/ImpFunctions.java
+++ Templates/ImpFunctions.java
@@ -26,4 +26,23 @@ public class ImpFunctions {
             System.out.println(ele.getKey() + " " + ele.getValue());
         }
     }
+
+    //Modulo division when mod value is prime
+    //(x / y) % mod = [x * y ^ (mod - 2)] % mod
+    static int mod = 1000 * 1000 * 1000 + 7;
+    public static int mul(int x, int y){
+        return (int)((x * 1l * y) % mod);
+    }
+    public static int power(int x, int y){
+        int ans = 1;
+        while(y > 0){
+            if((y & 1) == 1) ans = mul(ans, x);
+            x = mul(x, x);
+            y = y >> 1;
+        }
+        return ans;
+    }
+    public static int divide(int x, int y){
+        return mul(x, power(y, (mod - 2)));
+    }
 }
