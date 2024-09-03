338fbe431f8dccc954542464c5efe32dcd77e3f4
SinAyByCosAy <tanaysriofficial@gmail.com>
2024-09-04 01:32:58 +0530

    Phi of 1 to N

--- NumberTheory/EulerTotient.java
+++ NumberTheory/EulerTotient.java
@@ -1,12 +1,18 @@
 package DPBootcamp.NumberTheory;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EulerTotient {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        long result = n;
+        System.out.println(phi(n));
+        System.out.println(Arrays.toString(phiOneToN(n)));
+    }
+    public static int phi(int n){//can be also done using SPF
+        //TC: O(sqrt(N))
+        int result = n;
         for(int i = 2; i * i <= n; i++){
             if(n % i == 0){
                 result -= result / i;
@@ -16,7 +22,22 @@ public class EulerTotient {
         }
         if(n > 1)
             result -= result / n;
-        System.out.println(result);
+
+        return result;
+    }
+
+    public static int[] phiOneToN(int n){
+        //similar to sieve or SPF construction logic, TC: << O(NlogN)
+        int[] phiValues = new int[n + 1];
+        for(int i = 1; i <= n; i++)
+            phiValues[i] = i;
+
+        for(int i = 2; i <= n; i++){
+            if(phiValues[i] == i){
+                for(int j = i; j <= n; j += i)
+                    phiValues[j] -= phiValues[j] / i;
+            }
+        }
+        return phiValues;
     }
 }
-//TC: O(sqrt(N))
