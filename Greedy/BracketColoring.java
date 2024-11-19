d37345f4e3763eb5434b11ba4fb2f6cd5ecc0ed0
SinAyByCosAy <tanaysriofficial@gmail.com>
2024-11-19 19:31:54 +0530

    Bracket coloring - fix

--- Greedy/BracketColoring.java
+++ Greedy/BracketColoring.java
@@ -4,7 +4,7 @@ package DPBootcamp.Greedy;
 import java.util.Scanner;
 
 public class BracketColoring {
-    public static void main(String[] argsq){
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
         while(t-- > 0){
