de213d328384915ff709ebe8278ff5cc614aa7c6
SinAyByCosAy <tanaysriofficial@gmail.com>
2024-11-26 19:16:16 +0530

    Jump Game

---
+++ Combinatorics/JumpGame.java
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/jump-game/
+package DPBootcamp.Combinatorics;
+
+import java.util.Scanner;
+
+public class JumpGame {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+        System.out.println(canJump(arr));
+    }
+    public static boolean canJump(int[] nums){
+        int maxIndex = 0; //max limit till where our index can go
+        int n = nums.length;
+        for(int i = 0; i < n; i++){
+            if(i > maxIndex) return false; //we breached the limit
+            if(nums[i] + i > maxIndex) maxIndex = nums[i] + i; //if we can make a farther jump than before, we note it
+        }
+        return true;
+    }
+}
