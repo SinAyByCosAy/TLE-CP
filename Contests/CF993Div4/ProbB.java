9e84ed833ffb5a276347af8bc5bc28d6d6cb452c
SinAyByCosAy <tanaysriofficial@gmail.com>
2024-12-17 01:40:23 +0530

    Prob B

--- Contests/CF993Div4/ProbB.java
+++ Contests/CF993Div4/ProbB.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 public class ProbB {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- > 0){
-            int seats = sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            int c = sc.nextInt();
-            int res = 0;
-            int row1 = seats;
-            int row2 = seats;
-            res += Math.min(a, row1);
-            res += Math.min(b, row2);
-            int rem = row1 + row2 - res;
-            res += Math.min(rem, c);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for(int i = 0; i < n; i++){
+            String s = sc.nextLine();
+            int p1 = 0, p2 = s.length() - 1;
+            StringBuffer res = new StringBuffer();
+            for(int j = p2; j >= p1; j--){
+                char ch = s.charAt(j);
+                if(ch == 'p') ch = 'q';
+                else if(ch == 'q') ch = 'p';
+                res.append(ch);
+            }
             System.out.println(res);
         }
     }
