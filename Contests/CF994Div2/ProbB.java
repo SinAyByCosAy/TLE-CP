c9f976232db90f272770ca9742a700c5ddd9dd53
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-01-26 00:40:41 +0530

    Prob B

--- Contests/CF994Div2/ProbB.java
+++ Contests/CF994Div2/ProbB.java
@@ -1,6 +1,6 @@
+//https://codeforces.com/contest/2049/problem/B
 package DPBootcamp.Contests.CF994Div2;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ProbB {
@@ -11,35 +11,18 @@ public class ProbB {
             int n = sc.nextInt();
             sc.nextLine();
             String str = sc.nextLine();
-            int[] p = new int[n];
-            int[] s = new int[n];
-            Arrays.fill(p, Integer.MAX_VALUE);
-            Arrays.fill(s, Integer.MAX_VALUE);
-            for(int i = 0; i < n; i++){
+            int len = str.length();
+            int pCount = 0, sCount = 0;
+            for(int i = 0; i < len; i++){
                 char ch = str.charAt(i);
-                if(ch == 'p'){
-                    p[i] = i + 1;
-                }
-                if(ch == 's')
-                    s[i] = n - i;
+                if(ch == 's') sCount++;
+                else if(ch == 'p') pCount++;
             }
-            for(int i = 1; i < n; i++)
-                s[i] = Math.min(s[i], s[i - 1]);
-            for(int i = n - 2; i >= 0; i--)
-                p[i] = Math.min(p[i], p[i + 1]);
-            int[] ops = new int[n];
-            for(int i = 0; i < n; i++)
-                ops[i] = Math.min(p[i], s[i]);
-            Arrays.sort(ops);
-            boolean flag = true;
-            for(int i = 0; i < n; i++){
-                if(ops[i] < i + 1){
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) System.out.println("YES");
-            else System.out.println("NO");
+            if(sCount == 0 || pCount == 0) System.out.println("YES");
+            else if((sCount == 1 && str.charAt(0) == 's') || (pCount == 1 && str.charAt(len - 1) == 'p'))
+                System.out.println("YES");
+            else
+                System.out.println("NO");
         }
     }
 }
