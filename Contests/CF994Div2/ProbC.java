869babf11ace6e4599ed362dbfad703b1d1c504f
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-01-26 13:30:51 +0530

    Prob C done

--- Contests/CF994Div2/ProbC.java
+++ Contests/CF994Div2/ProbC.java
@@ -1,10 +1,10 @@
+//https://codeforces.com/contest/2049/problem/C
 package DPBootcamp.Contests.CF994Div2;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Set;
 
+//break into two cycles from x-y edge. mark x = 0, y = 1, mark all other nodes in cycle as 0/1 alternately
+//and if number of nodes in cycle is odd, mark the last node as 2
 public class ProbC {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
@@ -14,8 +14,25 @@ public class ProbC {
             int x = sc.nextInt();
             int y = sc.nextInt();
             int[] arr = new int[n];
-            Arrays.fill(arr, -1);
-            ArrayList<Set<Integer>> friends = new ArrayList<>();
+            arr[x - 1] = 0;
+            arr[y - 1] = 1;
+            int leftCycle = y - x + 1;
+            for(int i = x; i < y - 1; i++){
+                if(leftCycle % 2 == 1 && i == y - 2) arr[i] = 2;
+                else arr[i] = arr[i - 1] ^ 1;
+            }
+            int rightCycle = n - leftCycle;
+            int start = y;
+            int count = 1;
+            while(count <= rightCycle){
+                int idx = start % n;
+                if(rightCycle % 2 == 1 && count == rightCycle) arr[idx] = 2;
+                else arr[idx] = arr[(idx - 1 + n) % n] ^ 1;
+                start++;
+                count++;
+            }
+            for(int ele : arr) System.out.print(ele + " ");
+            System.out.println();
         }
     }
 }
