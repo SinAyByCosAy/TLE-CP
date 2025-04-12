6e3ad025a39d70786a686872c7e93a137676aff2
SinAyByCosAy <tanaysriofficial@gmail.com>
2025-04-12 16:33:02 +0530

    minor changes

--- DP/Knapsack2.java
+++ DP/Knapsack2.java
@@ -18,8 +18,9 @@ public class Knapsack2 {
         int limit = (int) 1e5;
         int[][] dp = new int[n + 1][limit + 1];
         for(int[] row : dp) Arrays.fill(row, w + 1); //setting value that can't be answer
+        dp[0][0] = 0;
         for(int i = 1; i <= n; i++){
-            dp[i - 1][0] = 0; //for 0 value, we need 0 weight
+            dp[i][0] = 0; //for 0 value, we need 0 weight
             for(int j = 1; j <= limit; j++){
                 dp[i][j] = dp[i - 1][j]; //don't pick current element
                 if(j >= value[i] && weights[i] + dp[i - 1][j - value[i]] <= w)
