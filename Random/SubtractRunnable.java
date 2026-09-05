ee4c84c995e4bb900b3afbb4a77b62002a42c472
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-09-05 23:59:56 +0530

    Client - Lock

--- Random/SubtractRunnable.java
+++ Random/SubtractRunnable.java
@@ -1,4 +1,21 @@
 package DPBootcamp.Random;
 
-public class SubtractRunnable {
+import java.util.concurrent.locks.Lock;
+
+public class SubtractRunnable implements Runnable{
+    CounterShareable cs;
+    Lock lock;
+    SubtractRunnable(CounterShareable cs, Lock lock){
+        this.cs = cs;
+        this.lock = lock;
+    }
+    @Override
+    public void run(){
+        for(int i = 0; i <= 10000; i++) {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " " + "Count="+cs.var);
+            cs.var -= i;
+            lock.unlock();
+        }
+    }
 }
