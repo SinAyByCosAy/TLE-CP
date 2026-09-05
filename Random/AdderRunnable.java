70a6903f9f901b9140aba8ce28fb1f1683cf23f7
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-09-05 23:59:10 +0530

    Adder function - Lock

--- Random/AdderRunnable.java
+++ Random/AdderRunnable.java
@@ -1,4 +1,21 @@
 package DPBootcamp.Random;
 
-public class AdderRunnable {
+import java.util.concurrent.locks.Lock;
+
+public class AdderRunnable implements Runnable{
+    CounterShareable cs;
+    Lock lock;
+    AdderRunnable(CounterShareable cs, Lock lock){
+        this.cs = cs;
+        this.lock = lock;
+    }
+    @Override
+    public void run(){
+        for(int i = 0; i <= 10000; i++) {
+            lock.lock();
+            System.out.println(Thread.currentThread().getName() + " " + "Count="+cs.var);
+            cs.var += i;
+            lock.unlock();
+        }
+    }
 }
