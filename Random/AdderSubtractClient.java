54db692a8ac1dd87e2c93a84ca48bd3cd49a975f
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-09-05 23:59:40 +0530

    Subtractor function - Lock

--- Random/AdderSubtractClient.java
+++ Random/AdderSubtractClient.java
@@ -1,4 +1,24 @@
 package DPBootcamp.Random;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class AdderSubtractClient {
+    public static void main(String[] args) throws InterruptedException {
+        CounterShareable cs = new CounterShareable();
+        Lock lock = new ReentrantLock();
+
+        AdderRunnable ar = new AdderRunnable(cs, lock);
+        SubtractRunnable sr = new SubtractRunnable(cs, lock);
+        Thread t1 = new Thread(ar);
+        Thread t2 = new Thread(sr);
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(cs.var);
+    }
 }
