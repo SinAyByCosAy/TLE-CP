9333cdb2e19657d38a9c88509645485d0d6f1f87
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-09-12 17:17:35 +0530

    Producer Consumer - lock

--- Random/ProducerConsumer/Consumer.java
+++ Random/ProducerConsumer/Consumer.java
@@ -1,4 +1,23 @@
 package DPBootcamp.Random.ProducerConsumer;
 
-public class Consumer {
+import java.util.Queue;
+
+public class Consumer implements Runnable{
+    int size;
+    Queue<Object> shelf;
+    Consumer(Queue<Object> shelf, int size){
+        this.shelf = shelf;
+        this.size = size;
+    }
+    @Override
+    public void run(){
+        while(true){
+            synchronized(shelf) {
+                if (shelf.size() > 0) {
+                    System.out.println(shelf.size() + " " + Thread.currentThread().getName());
+                    shelf.remove();
+                }
+            }
+        }
+    }
 }
--- Random/ProducerConsumer/Producer.java
+++ Random/ProducerConsumer/Producer.java
@@ -1,4 +1,23 @@
 package DPBootcamp.Random.ProducerConsumer;
 
-public class Producer {
+import java.util.Queue;
+
+public class Producer implements Runnable{
+    int size;
+    Queue<Object> shelf;
+    Producer(Queue<Object> shelf, int size){
+        this.shelf = shelf;
+        this.size = size;
+    }
+    @Override
+    public void run(){
+        while(true){
+            synchronized (shelf) {
+                if (shelf.size() < size) {
+                    System.out.println(shelf.size() + " " + Thread.currentThread().getName());
+                    shelf.add(new Object());
+                }
+            }
+        }
+    }
 }
