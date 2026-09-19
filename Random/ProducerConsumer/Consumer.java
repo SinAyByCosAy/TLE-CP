cf791e6eb9605c472ce212768ad075c45455f637
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-09-19 19:41:32 +0530

    Concurrent DS: Consumer

--- Random/ProducerConsumer/Consumer.java
+++ Random/ProducerConsumer/Consumer.java
@@ -1,29 +1,41 @@
 package DPBootcamp.Random.ProducerConsumer;
 
 import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Semaphore;
 
 public class Consumer implements Runnable{
-    int size;
-    Queue<Object> shelf;
-    Semaphore ps, cs;
-    Consumer(Queue<Object> shelf, int size, Semaphore ps, Semaphore cs){
-        this.shelf = shelf;
-        this.size = size;
-        this.ps = ps;
-        this.cs = cs;
+//    int size;
+//    Queue<Object> shelf;
+//    Semaphore ps, cs;
+//    Consumer(Queue<Object> shelf, int size, Semaphore ps, Semaphore cs){
+//        this.shelf = shelf;
+//        this.size = size;
+//        this.ps = ps;
+//        this.cs = cs;
+//    }
+
+    BlockingQueue<Object> queue;
+    Consumer(BlockingQueue<Object> queue){
+        this.queue = queue;
     }
     @Override
     public void run(){
         while(true){
-            try {
-                cs.acquire();
-                System.out.println(shelf.size() + " " + Thread.currentThread().getName());
-                shelf.remove();
-                ps.release();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            try{
+                queue.take();
+                System.out.println("Consumer: "+Thread.currentThread().getName()+" size: "+queue.size());
+            }catch (Exception e){
+                System.out.println(e);
             }
+//            try {
+//                cs.acquire();
+//                System.out.println(shelf.size() + " " + Thread.currentThread().getName());
+//                shelf.remove();
+//                ps.release();
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
         }
     }
 }
