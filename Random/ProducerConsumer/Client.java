5e79621045442d444fccd09e229f1d014804f8bb
SinAyByCosAy <tanaysriofficial@gmail.com>
2026-09-19 19:41:03 +0530

    Concurrent DS: client

--- Random/ProducerConsumer/Client.java
+++ Random/ProducerConsumer/Client.java
@@ -2,16 +2,23 @@ package DPBootcamp.Random.ProducerConsumer;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Semaphore;
 
 public class Client {
     public static void main(String args[]){
-        Queue<Object> shelf = new LinkedList<>();
         int size = 5;
-        Semaphore ps = new Semaphore(size);
-        Semaphore cs = new Semaphore(0);
-        Producer producer = new Producer(shelf, size, ps, cs);
-        Consumer consumer = new Consumer(shelf, size, ps, cs);
+//        Queue<Object> shelf = new LinkedList<>();
+//        Semaphore ps = new Semaphore(size);
+//        Semaphore cs = new Semaphore(0);
+//        Producer producer = new Producer(shelf, size, ps, cs);
+//        Consumer consumer = new Consumer(shelf, size, ps, cs);
+
+        BlockingQueue<Object> queue = new ArrayBlockingQueue(size);
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+
         Thread p1 = new Thread(producer);
         Thread p2 = new Thread(producer);
         Thread p3 = new Thread(producer);
